3fa4c45e2e3b0b2b7a0c90ddc1ea32de00c0594b
andriyn <Andriy.Nikulshin@nice.com>
2018-05-04 19:25:26 +0300

    ddd

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,45 +1,21 @@
-// import {Component, OnInit} from '@angular/core';
-// import {SearchMusicService} from './search-music.service';
-// import {SearchData} from './searchResult';
-//
-// @Component({
-//   selector: 'app-root',
-//   templateUrl: './app.component.html',
-//   styleUrls: ['./app.component.css']
-// })
-// export class AppComponent implements OnInit {
-//  searchResultfromComponent: SearchData;
-//  searchInput = 'Search Input';
-//  searchImage = 'https://i1.sndcdn.com/artworks-000059522031-nwy1v0-large.jpg';
-//
-//   constructor(private searchServicefromComponent: SearchMusicService) {
-//     this.searchResultfromComponent = new SearchData ();
-//     this.searchResultfromComponent.searchResultName.push('Please, enter search data');
-//     this.searchResultfromComponent.searchResultImage.push('https://i1.sndcdn.com/artworks-000059522031-nwy1v0-large.jpg');
-//   }
-//
-//   ngOnInit() {
-//     this.searchServicefromComponent.getPost(this.searchInput).subscribe(posts => {
-//       console.log(posts);
-//        this.searchResultfromComponent.searchResultName = posts;
-//     });
-//   }
-// }
 import {Component, OnInit} from '@angular/core';
 import {SearchMusicService} from './search-music.service';
 import {SearchData} from './searchResult';
-import index from '@angular/cli/lib/cli';
+
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
+
 export class AppComponent implements OnInit {
   searchResultfromComponent: SearchData;
   searchInput = 'The Best';
   searchImage = 'https://i1.sndcdn.com/artworks-000059522031-nwy1v0-large.jpg';
   searchhistory = new Array();
+  SC = require('soundcloud');
+
  // let searchhistory = new Array(3);
 
   constructor(private searchServicefromComponent: SearchMusicService) {
@@ -47,6 +23,8 @@ export class AppComponent implements OnInit {
     this.searchResultfromComponent.searchResultName.push('Please, enter search data');
     this.searchResultfromComponent.searchResultImage.push('https://i1.sndcdn.com/artworks-000059522031-nwy1v0-large.jpg');
   }
+
+
 
   ngOnInit() {
     this.searchServicefromComponent.getPost(this.searchInput).subscribe(posts => {
@@ -60,6 +38,12 @@ export class AppComponent implements OnInit {
     this.searchImage = this.searchResultfromComponent.searchResultImage[n];
   }
 
+  playTrack (n: number) {
+    this.SC.initialize({client_id: 'ggX0UomnLs0VmW7qZnCzw'});
+    let track_url = 'https://soundcloud.com/pr-abdel-rahman-p/the-best-songs-love';
+    this.SC.oEmbed(track_url, {auto_play: false}).then(function (oEmbed) {
+      console.log('oEmbed response: ', oEmbed);
+    });
 
-
+  }
 }
